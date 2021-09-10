faf3667d97896927ebc50cfc468c55835ba759e2
toniz <toniz7960@gmail.com>
2021-09-10 18:49:58 +0800

    add

--- demo/pack1/pack1.go
+++ demo/pack1/pack1.go
@@ -5,12 +5,12 @@ import (
     "errors"
 
     tracelog "github.com/toniz/otel"
-    pack2 "github.com/toniz/otel/demo/pack2"
+//    pack2 "github.com/toniz/otel/demo/pack2"
 )
 
 func CallLevel_1(ctx context.Context) string {
     ctxc, _ := tracelog.NewSpan(ctx, "pack1.CallLevel_1", OtelSpanKindProducer)
-    result := "-> 1" + pack.CallLevel_2(ctxc)
+//    result := "-> 1" + pack.CallLevel_2(ctxc)
 
     tracelog.SetSpanOK(ctxc, "Successssssss.")
     tracelog.AddSpanAttribute(ctxc, map[string]string{"user_id": "1000098"})
